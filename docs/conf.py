bed900f304b94760593694bbdadcc24a0220b7e3
Bas Hoonhout <bas.hoonhout@deltares.nl>
2018-03-02 13:26:52 +0100

    See if this fixes RTD processing...

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['cv2']
+MOCK_MODULES = ['cv2','videoslicer']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
